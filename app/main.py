6ec404cd2c515302b2e5f1f2f8ba1897142d1e28
aarya1-spec <aaryaniraula62@gmail.com>
2026-08-28 12:14:18 +0545

    Add cookie expiry

--- app/main.py
+++ app/main.py
@@ -13,7 +13,13 @@ def hello(request: Request, response: Response):
     user_id = request.cookies.get("user_id")
     if not user_id:
         user_id = str(uuid.uuid4())
-        response.set_cookie("user_id", user_id)
+        response.set_cookie(
+            key="user_id",
+            value=user_id,
+            max_age=60 * 60 * 24 * 30,
+            httponly=True,
+            samesite="lax"
+        )
 
     count = r.incr(f"user:{user_id}")
 
